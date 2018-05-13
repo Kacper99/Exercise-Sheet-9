536ba396c6e781fc4be477d7714227ff24ba9e9c
Kacper Martela <kacper99@hotmail.co.uk>
2018-05-13 23:01:25 +0100

    Added comments

--- q1.c
+++ q1.c
@@ -2,10 +2,10 @@
 #include <math.h>
 
 int analyse(int *wholePtr, double *fracPtr, double d) {
-    if (d < 0) {
-        d += d * -2;
-        *wholePtr = (int) d;
-        *fracPtr = d - *wholePtr;
+    if (d < 0) { //If the number is below we will return -1, else we will return 1
+        d += d * -2; //As the required output is to show the fractional and whole part without the minus we have to turn into a positive number
+        *wholePtr = (int) d; //Casting the double to an int will always round down
+        *fracPtr = d - *wholePtr; //We take away the whole part and get left with the fractional part
         return -1;
     } else {
         *wholePtr = (int) d;
