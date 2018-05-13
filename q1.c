b70ddcc4f7d13ae8ad9ddfbed560ca2c0466ba73
Kacper Martela <kacper99@hotmail.co.uk>
2018-05-13 22:57:18 +0100

    Removed redundant code

--- q1.c
+++ q1.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-/*
 int analyse(int *wholePtr, double *fracPtr, double d) {
-    *fracPtr = (int) modf(d, wholePtr);
-
-    if (d < 0) {
-        return -1;
-    } else {
-        return 1;
-    }
-}
-*/
-
-int analyse(int *wholePtr, double *fracPtr, double d) {
-    
-
     if (d < 0) {
         d += d * -2;
         *wholePtr = (int) d;
@@ -29,12 +15,11 @@ int analyse(int *wholePtr, double *fracPtr, double d) {
 }
 
 int main(int argc, char const *argv[]) {
-    int sign;
-    //double input, fracPart, wholePart;
+    int sign, wholePart;
     double input, fracPart;
-    int wholePart;
+
     printf("What number would you like to analyse?\n");
-    scanf("%lf", &input); //Check if I can use without the &
+    scanf("%lf", &input);
 
     sign = analyse(&wholePart, &fracPart, input);
 
