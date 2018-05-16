03b147115719692542630ecda95a8376c5d09576
Kacper Martela <kacper99@hotmail.co.uk>
2018-05-16 17:58:29 +0100

    Created constructor for triangular array

--- q2.c
+++ q2.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef int* TRIARR;
+
+/*
+Valid only when rows <= columns
+e.g. [1,3], [2,3], [3,3] are valid but [4,3] wouldn't be
+*/
+
+TRIARR triNew(int n) 
+{
+    TRIARR array = malloc(((n*(n + 1))/2) * sizeof(int)); //The size will be the triangular number times the size of integer
+    if (array = NULL) //Print error message 
+    {
+        printf("Failed to allocate memory\n");
+    }
+    return (int)* array;
+}
 
 int main(int argc, char const *argv[])
 {
