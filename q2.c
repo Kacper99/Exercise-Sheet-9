d64cb5db1756434a6e30645005a38d43287d4b64
Kacper Martela <kacper99@hotmail.co.uk>
2018-05-16 19:25:05 +0100

    Wrote triStore and triFetch

--- q2.c
+++ q2.c
@@ -11,15 +11,48 @@ e.g. [1,3], [2,3], [3,3] are valid but [4,3] wouldn't be
 TRIARR triNew(int n) 
 {
     TRIARR array = malloc(((n*(n + 1))/2) * sizeof(int)); //The size will be the triangular number times the size of integer
-    if (array = NULL) //Print error message 
+    if (array == NULL) //Print error message 
     {
         printf("Failed to allocate memory\n");
     }
-    return (int)* array;
+    return array; //Return pointer to the start of array
+}
+
+int triStore(TRIARR as, int N, int row, int col, int val) 
+{
+    //Row <= column, row and column must be less than N
+    if (row <= col && row < N && col < N)
+    {
+        *(as + (row * sizeof(int)) + (col * sizeof(int))) = val;
+        return 1;
+    }
+    else 
+    {
+        return -1;
+    }
+    
+}
+
+int triFetch(TRIARR as, int N, int row, int col)
+{
+    if (row <= col && row < N && col < N) 
+    {
+        int val = *(as + (row * sizeof(int)) + (col * sizeof(int)));
+        printf("Value at %i , %i is %i", row, col, val);
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    
+    int size = 6;
+    TRIARR as = triNew(6);
+
+    triStore(as, size, 7, 7, 10);
+    triFetch(as, size, 7, 7);
     return 0;
 }
