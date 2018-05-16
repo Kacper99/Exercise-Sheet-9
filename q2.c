408a582d5bdbdc6d2dd65a7544fb0ba000d7c896
Kacper Martela <kacper99@hotmail.co.uk>
2018-05-16 23:53:40 +0100

    Implemented remaining methods

--- q2.c
+++ q2.c
@@ -10,7 +10,7 @@ e.g. [1,3], [2,3], [3,3] are valid but [4,3] wouldn't be
 
 TRIARR triNew(int n) 
 {
-    TRIARR array = malloc(((n*(n + 1))/2) * sizeof(int)); //The size will be the triangular number times the size of integer
+    TRIARR array = (int*) malloc(((n*(n + 1))/2) * sizeof(int)); //The size will be the triangular number times the size of integer
     if (array == NULL) //Print error message 
     {
         printf("Failed to allocate memory\n");
@@ -20,25 +20,23 @@ TRIARR triNew(int n)
 
 int triStore(TRIARR as, int N, int row, int col, int val) 
 {
-    //Row <= column, row and column must be less than N
     if (row <= col && row < N && col < N)
     {
-        *(as + (row * sizeof(int)) + (col * sizeof(int))) = val;
+        *(as + row + col) = val;
         return 1;
     }
     else 
     {
         return -1;
     }
-    
 }
-
+    
 int triFetch(TRIARR as, int N, int row, int col)
 {
     if (row <= col && row < N && col < N) 
     {
-        int val = *(as + (row * sizeof(int)) + (col * sizeof(int)));
-        printf("Value at %i , %i is %i", row, col, val);
+        int val = *(as + row + col);
+        printf("Value at %i , %i is %i \n", row, col, val);
         return 1;
     }
     else
@@ -49,25 +47,23 @@ int triFetch(TRIARR as, int N, int row, int col)
 
 int main(int argc, char const *argv[])
 {
-    int size = 6;
-    TRIARR as = triNew(6);
-
+    int size = 10;
+    TRIARR as = triNew(size);
     for(int i = 0;i < size;i++)
     {
-        for(int j = 0;j < size;i++)
+        for(int j = 0; j < i; j++) 
         {
-            triStore(as, size, i, j, i*j);    
-        }
+            triStore(as, size, j, i, i * 2);
+        }        
     }
 
     for(int i = 0;i < size;i++)
     {
-        for(int j = 0;j < size;i++)
+        for(int j = 0; j < i; j++) 
         {
-            triFetch(as, size, i, j);
-        }
+            triFetch(as, size, j, i);
+        }        
     }
 
-    triFetch(as, size, 7, 7);
     return 0;
 }
